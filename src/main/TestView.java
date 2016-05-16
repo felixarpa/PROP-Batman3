eca3f13a7189e70369135223488fd7dd40c921fd
EnderRL <carlosroldanmontaner@gmail.com>
2016-05-16 12:39:58 +0200

    lol

--- src/main/TestView.java
+++ src/main/TestView.java
@@ -4,13 +4,15 @@ import presentation.Presenter;
 import view.BaseView;
 import view.MyApp;
 
+import javax.swing.text.View;
+
 public class TestView extends Presenter{
 
     private BaseView view;
 
     public TestView() {
         view = new BaseView(this);
-        MyApp.startScene(BaseView.getContent());
+        MyApp.startScene(view.getContent());
     }
 
 }
