113a5c51ee8a33d0c8628be51121d19292866004
Mario <mario_bcn2@hotmail.es>
2016-05-24 19:35:28 +0200

    Esto empieza arular

--- src/presentation/FilteredSearchResultPresenter.java
+++ src/presentation/FilteredSearchResultPresenter.java
@@ -1,5 +1,6 @@
 package presentation;
 
+import view.Config;
 import view.FilteredSearchResultView;
 import view.MyApp;
 
@@ -20,6 +21,8 @@ public class FilteredSearchResultPresenter  extends BasePresenter {
     public void setType(int type) {
         index = 0;
         actualType = type;
+        ((FilteredSearchResultView)actualView).changeType(type);
+        show();
     }
 
     private void initialFill(int i) {
@@ -28,34 +31,42 @@ public class FilteredSearchResultPresenter  extends BasePresenter {
         int nextIndex = FilteredSearchResultView.numToShow;
 
         for (int index = 0; index < max; ++index) {
-            ((FilteredSearchResultView) actualView).setContent(index, result.get(i).get(index), i);
+            ((FilteredSearchResultView) actualView).setContent(index, result.get(i).get(index), i,FilteredSearchResultView.numToShow);
         }
 
         for (;max < nextIndex; ++max) {
-            ((FilteredSearchResultView) actualView).setContent(max, "\t \t \t \t",i);
+            ((FilteredSearchResultView) actualView).setContent(max, "\t \t \t \t",i,FilteredSearchResultView.numToShow);
         }
     }
 
     public void showMore() {
-        int max = index + FilteredSearchResultView.numToShow;
-        if (max > result.get(actualType).size()) max = result.get(actualType).size();
-        int nextIndex = index + FilteredSearchResultView.numToShow;
+        if (index + Config.LISTS_SIZE <= result.size()) {
+            index += Config.LISTS_SIZE;
+            show();
 
-        for (; index < max; ++index) {
-            ((FilteredSearchResultView) actualView).setContent(index, result.get(actualType).get(index), 0);
         }
 
-        for (;max < nextIndex; ++max) {
-            ((FilteredSearchResultView) actualView).setContent(max, "\t \t \t \t",0);
-        }
     }
 
     public void showLess() {
-
-        for (; index >= 0; --index) {
-            ((FilteredSearchResultView) actualView).setContent(index, result.get(actualType).get(index),0);
+        if (index - Config.LISTS_SIZE >= 0) {
+            index = index - Config.LISTS_SIZE;
+            show();
         }
 
+
+    }
+
+    private void show() {
+        int max = index+ Config.LISTS_SIZE;
+        if (max> result.get(actualType).size()) max = result.get(actualType).size();
+        for (int i = 0; i < max-index; ++i) {
+            System.out.println(i);
+            ((FilteredSearchResultView)actualView).setContent(index+i, result.get(actualType).get(index+i),actualType,Config.LISTS_SIZE);
+        }
+        for (;max < index+Config.LISTS_SIZE; ++max) {
+            ((FilteredSearchResultView)actualView).setContent(max, "\t \t \t \t",actualType,Config.LISTS_SIZE);
+        }
     }
 
 }
--- src/view/FilteredSearchResultView.java
+++ src/view/FilteredSearchResultView.java
@@ -1,9 +1,11 @@
 package view;
 
 
+import javafx.event.EventHandler;
 import javafx.geometry.*;
 import javafx.scene.Node;
 import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
@@ -156,31 +158,134 @@ public class FilteredSearchResultView extends BaseView {
     }
 
     private void setListeners() {
+        authorText.setOnMouseReleased(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                ((FilteredSearchResultPresenter)presenter).setType(ProjectConstants.AUTHOR_TYPE);
+            }
+        });
+        conferenceText.setOnMouseReleased(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                ((FilteredSearchResultPresenter)presenter).setType(ProjectConstants.CONFERENCE_TYPE);
+            }
+        });
+        paperText.setOnMouseReleased(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                ((FilteredSearchResultPresenter)presenter).setType(ProjectConstants.PAPER_TYPE);
+            }
+        });
+        termText.setOnMouseReleased(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                ((FilteredSearchResultPresenter)presenter).setType(ProjectConstants.TERM_TYPE);
+            }
+        });
     }
 
-    public void setContent(int index, String node, int type) {
+
+    public void setContent(int index, String node, int type, int listSize) {
         String[] elements = node.split("\t");
-        number.get(type).get(index%numToShow).setText(Integer.toString(index+1));
-        number.get(type).get(index%numToShow).setMinWidth(10);
-        number.get(type).get(index%numToShow).setMaxWidth(10);
-        //number.get(type).get(index%numToShow).setPadding(new Insets(0,50,0,0));
-        name.get(type).get(index%numToShow).setText(elements[0]);
-        name.get(type).get(index%numToShow).setMinWidth(400);
-        name.get(type).get(index%numToShow).setMaxWidth(400);
-        //name.get(type).get(index%numToShow).setPadding(new Insets(0,50,0,0));
-        id.get(type).get(index%numToShow).setText("Id: " + elements[1]);
-        id.get(type).get(index%numToShow).setMinWidth(100);
-        id.get(type).get(index%numToShow).setMaxWidth(100);
-        //id.get(type).get(index%numToShow).setPadding(new Insets(0,50,0,0));
-        relevance.get(type).get(index%numToShow).setText("Relevance: " + elements[2]);
-        relevance.get(type).get(index%numToShow).setMinWidth(300);
-        relevance.get(type).get(index%numToShow).setMaxWidth(300);
-        //relevance.get(type).get(index%numToShow).setPadding(new Insets(0,50,0,0));
-        label.get(type).get(index%numToShow).setText("Label: " + elements[3]);
-        label.get(type).get(index%numToShow).setMinWidth(75);
-        label.get(type).get(index%numToShow).setMaxWidth(75);
-        //label.get(type).get(index%numToShow).setPadding(new Insets(0,50,0,0));
+        if (!elements[0].equals("")) {
+            number.get(type).get(index%listSize).setText(Integer.toString(index+1));
+            number.get(type).get(index%listSize).setMinWidth(50);
+            number.get(type).get(index%listSize).setMaxWidth(50);
+            //number.get(type).get(index%listSize).setPadding(new Insets(0,0,0,0));
+            id.get(type).get(index%listSize).setText("Id: " + elements[1]);
+            id.get(type).get(index%listSize).setMinWidth(100);
+            id.get(type).get(index%listSize).setMaxWidth(100);
+            //id.get(type).get(index%listSize).setPadding(new Insets(0,50,0,0));
+            relevance.get(type).get(index%listSize).setText("Relevance: " + elements[2]);
+            relevance.get(type).get(index%listSize).setMinWidth(300);
+            relevance.get(type).get(index%listSize).setMaxWidth(300);
+            //relevance.get(type).get(index%listSize).setPadding(new Insets(0,50,0,0));
+
+            label.get(type).get(index%listSize).setText("Label: " + elements[3]);
+            label.get(type).get(index%listSize).setMinWidth(75);
+            label.get(type).get(index%listSize).setMaxWidth(75);
+            //label.get(type).get(index%listSize).setPadding(new Insets(0,50,0,0));
+        }
+        else {
+            number.get(type).get(index%listSize).setText("");
+            number.get(type).get(index%listSize).setMinWidth(10);
+            number.get(type).get(index%listSize).setMaxWidth(10);
+            //number.get(type).get(index%listSize).setPadding(new Insets(0,50,0,0));
+            id.get(type).get(index%listSize).setText("");
+            id.get(type).get(index%listSize).setMinWidth(100);
+            id.get(type).get(index%listSize).setMaxWidth(100);
+            //id.get(type).get(index%listSize).setPadding(new Insets(0,50,0,0));
+            relevance.get(type).get(index%listSize).setText("");
+            relevance.get(type).get(index%listSize).setMinWidth(300);
+            relevance.get(type).get(index%listSize).setMaxWidth(300);
+            //relevance.get(type).get(index%listSize).setPadding(new Insets(0,50,0,0));
+            label.get(type).get(index%listSize).setText("");
+            label.get(type).get(index%listSize).setMinWidth(75);
+            label.get(type).get(index%listSize).setMaxWidth(75);
+            //label.get(type).get(index%listSize).setPadding(new Insets(0,50,0,0));
+        }
+        name.get(type).get(index%listSize).setText(elements[0]);
+        name.get(type).get(index%listSize).setMinWidth(400);
+        name.get(type).get(index%listSize).setMaxWidth(400);
+        //name.get(type).get(index%listSize).setPadding(new Insets(0,50,0,0));
 
     }
+
+    public void changeType(int type) {
+        for (int i = 0; i < Config.LISTS_SIZE - numToShow; ++i) {
+            ArrayList<Label> laux = new ArrayList<>(5);
+            for (int j = 0; j < 5; ++j) {
+                Label l = new Label();
+                l.setFont(new Font(20));
+                l.setTextFill(Paint.valueOf("white"));
+                l.setFont(font);
+                //l.setFont(new Font("Microsoft Sans Serif",15));
+                laux.add(l);
+            }
+            number.get(type).add(laux.get(0));
+            name.get(type).add(laux.get(1));
+            id.get(type).add(laux.get(2));
+            relevance.get(type).add(laux.get(3));
+            label.get(type).add(laux.get(4));
+        }
+        contentVBox.getChildren().removeAll(contents);
+        VBox vaux = new VBox();
+
+        HBox haux = new HBox();
+        haux.setPadding(new Insets(0,0,0,50));
+
+        switch (type) {
+            case ProjectConstants.AUTHOR_TYPE:
+                haux.getChildren().add(authorText);
+                authorText.setOnMouseReleased(null);
+                break;
+            case ProjectConstants.CONFERENCE_TYPE:
+                haux.getChildren().add(conferenceText);
+                conferenceText.setOnMouseReleased(null);
+                break;
+            case ProjectConstants.PAPER_TYPE:
+                haux.getChildren().add(paperText);
+                paperText.setOnMouseReleased(null);
+                break;
+            case ProjectConstants.TERM_TYPE:
+                haux.getChildren().add(termText);
+                termText.setOnMouseReleased(null);
+                break;
+        }
+        vaux.getChildren().add(haux);
+        for (int j = 0; j < Config.LISTS_SIZE; ++j) {
+            haux = new HBox();
+            haux.setPadding(new Insets(4,0,4,50));
+            haux.getChildren().add(number.get(type).get(j));
+            haux.getChildren().add(name.get(type).get(j));
+            haux.getChildren().add(label.get(type).get(j));
+            haux.getChildren().add(id.get(type).get(j));
+            haux.getChildren().add(relevance.get(type).get(j));
+            vaux.getChildren().add(haux);
+
+        }
+        contentVBox.getChildren().add(vaux);
+    }
+
 
 }
