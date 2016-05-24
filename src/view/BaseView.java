408ba52a875a098ce0006856a988d133834da246
unknown <carlosroldanmontaner@gmail.com>
2016-05-24 18:07:35 +0200

    'dgfasdg'

--- src/view/BaseView.java
+++ src/view/BaseView.java
@@ -194,7 +194,7 @@ public class BaseView {
         appTitleImage.setFitWidth(409);
         appTitleImage.setFitHeight(106);
 
-        authorsButton = new  ImageButton("../images", "authorsButton", 225, 75);
+        authorsButton = new  ImageButton("../images", "authorsButton", 220, 75);
 
         conferencesButton = new ImageButton("../images", "conferencesButton", 225, 75);
 
