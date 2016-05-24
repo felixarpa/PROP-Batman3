3182c36a55f8214ad31ac3bf458d313459755e06
Daryan7 <juanmiguel1996@gmail.com>
2016-05-24 16:20:39 +0200

    asdasd

--- src/presentation/FilteredSearchResultPresenter.java
+++ src/presentation/FilteredSearchResultPresenter.java
@@ -2,44 +2,59 @@ package presentation;
 
 import view.FilteredSearchResultView;
 import view.MyApp;
-import view.RelevanceTypeSelectorView;
 
 import java.util.ArrayList;
 
 public class FilteredSearchResultPresenter  extends BasePresenter {
 
     ArrayList<ArrayList<String>> result;
-    ArrayList<Integer> index;
+    int index, actualType;
 
     public FilteredSearchResultPresenter(ArrayList<ArrayList<String>> result) {
         this.result = result;
         actualView = new FilteredSearchResultView(this);
-        index = new ArrayList<>(4);
-        for (int i = 0; i < 4; ++i) index.add(0);
-        for (int i = 0; i < 4; ++i) showMore(i);
+        for (int i = 0; i < 4; ++i) initialFill(i);
         MyApp.startScene(actualView.getContent());
     }
 
-    public void showMore(int i) {
-        int max = index.get(i) + FilteredSearchResultView.numToShow;
-        if (max > result.get(i).size()) max = result.get(i).size();
-        int nextindex = index.get(i) + FilteredSearchResultView.numToShow;
+    public void setType(int type) {
+        actualType = type;
+    }
 
-        for (; index.get(i) < max; index.set(i, index.get(i)+1)) {
-            ((FilteredSearchResultView) actualView).setContent(index.get(i), result.get(i).get(index.get(i)), i);
+    private void initialFill(int i) {
+        int max = FilteredSearchResultView.numToShow;
+        if (max > result.get(i).size()) max = result.get(i).size();
+        int nextIndex = FilteredSearchResultView.numToShow;
+
+        for (int index = 0; index < max; ++index) {
+            ((FilteredSearchResultView) actualView).setContent(index, result.get(i).get(index), i);
         }
 
-        for (;max < nextindex; ++max) {
+        for (;max < nextIndex; ++max) {
             ((FilteredSearchResultView) actualView).setContent(max, "\t \t \t \t",i);
         }
     }
 
-    public void showLess(int i) {
-        int min = index.get(i)-FilteredSearchResultView.numToShow;
+    public void showMore() {
+        int max = index + FilteredSearchResultView.numToShow;
+        if (max > result.get(actualType).size()) max = result.get(actualType).size();
+        int nextIndex = index + FilteredSearchResultView.numToShow;
 
-        for (; index.get(i) >= 0; index.set(i, index.get(i)-1)) {
-            ((FilteredSearchResultView) actualView).setContent(index.get(i), result.get(i).get(index.get(i)),i);
+        for (; index < max; ++index) {
+            ((FilteredSearchResultView) actualView).setContent(index, result.get(actualType).get(index), 0);
         }
+
+        for (;max < nextIndex; ++max) {
+            ((FilteredSearchResultView) actualView).setContent(max, "\t \t \t \t",0);
+        }
+    }
+
+    public void showLess() {
+
+        for (; index >= 0; --index) {
+            ((FilteredSearchResultView) actualView).setContent(index, result.get(actualType).get(index),0);
+        }
+
     }
 
 }
