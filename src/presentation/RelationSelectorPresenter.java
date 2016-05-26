65791a881f5c2e6a40894afeafb1406adbd05cc1
Mario <mario_bcn2@hotmail.es>
2016-05-26 11:27:26 +0200

    RelationSelectorPresenterDone

--- src/presentation/RelationSelectorPresenter.java
+++ src/presentation/RelationSelectorPresenter.java
@@ -1,12 +1,27 @@
 package presentation;
 
+import domain.DomainController;
+import util.ProjectConstants;
 import view.MyApp;
 import view.RelationSelectorView;
 
+import java.util.ArrayList;
+
 public class RelationSelectorPresenter extends BasePresenter {
+
     public RelationSelectorPresenter() {
         actualView = new RelationSelectorView(this);
         MyApp.startScene(actualView.getContent());
+    }
 
+    public void onClickSearch() {
+        int type1 = ((RelationSelectorView)actualView).getFirstType();
+        int type2 = ((RelationSelectorView)actualView).getSecondType();
+        if (type1 == type2 && type1 == ProjectConstants.PAPER_TYPE) ((RelationSelectorView)actualView).showError();
+        else {
+            actualView.destroy();
+            actualView = null;
+            MyApp.changePresenter(new RelationshipRelevanceResultPresenter(type1,type2));
+        }
     }
 }
