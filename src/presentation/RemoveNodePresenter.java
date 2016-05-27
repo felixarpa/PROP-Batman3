55abf0a7dbb7abbf0ce974ddf050302ea35bdffc
Daryan7 <mario_bcn2@hotmail.es>
2016-05-27 18:16:49 +0200

    asadsa

--- src/presentation/RemoveNodePresenter.java
+++ src/presentation/RemoveNodePresenter.java
@@ -4,9 +4,8 @@ import domain.DomainController;
 import view.MyApp;
 import view.RemoveNodeView;
 
-/**
- * Created by carlos.roldan on 26/05/2016.
- */
+import java.util.ArrayList;
+
 public class RemoveNodePresenter extends MainAdminPresenter {
 
     public RemoveNodePresenter() {
@@ -16,5 +15,19 @@ public class RemoveNodePresenter extends MainAdminPresenter {
 
     public void clickRemoveNodeButton() {
         System.out.println("Haz el presenter Juanmi (ﾉಠдಠ)ﾉ︵┻━┻");
+        String nombre = ((RemoveNodeView)actualView).getSearchText();
+        ((RemoveNodeView)actualView).startProgress();
+        Thread thread = new Thread(() -> {
+            ArrayList<String> result = domainController.searchingANode(nombre);
+            if (result == null) {
+                System.out.println("No results found");
+                ((RemoveNodeView)actualView).stopProgress();
+                return;
+            }
+            actualView.destroy();
+            actualView = null;
+            //MyApp.changePresenter();
+        });
+        thread.start();
     }
 }
