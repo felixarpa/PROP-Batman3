41ca0f0c31b5cd965bddab577a89c86ac4ade48c
mariofv <mario_bcn2@hotmail.es>
2016-05-27 13:34:10 +0200

    Hola:D

---
+++ src/presentation/AddFavoriteTermSearchPresenter.java
@@ -0,0 +1,25 @@
+package presentation;
+
+import presentation.MainPresenter;
+import view.AddFavoriteTermSearchView;
+import view.MainView;
+import view.MyApp;
+
+import java.util.ArrayList;
+
+public class AddFavoriteTermSearchPresenter extends MainPresenter{
+    
+    public AddFavoriteTermSearchPresenter() {
+        actualView = new AddFavoriteTermSearchView(this);
+        MyApp.startScene(actualView.getContent());
+    }
+
+    @Override
+    public void clickSearchButton() {
+        String nom = ((AddFavoriteTermSearchView)actualView).getSearchText();
+        ArrayList<String> result = domainController.searchingATerm(nom);
+        actualView.destroy();
+        actualView = null;
+        //CAMBIAR A SELECCIONAR TEMA
+    }
+}
---
+++ src/view/AddFavoriteTermSearchView.java
@@ -0,0 +1,13 @@
+package view;
+
+
+import presentation.AddFavoriteTermSearchPresenter;
+
+public class AddFavoriteTermSearchView extends MainView{
+    public AddFavoriteTermSearchView(AddFavoriteTermSearchPresenter addFavoriteTermSearchPresenter) {
+        super(addFavoriteTermSearchPresenter);
+
+    }
+
+
+}
