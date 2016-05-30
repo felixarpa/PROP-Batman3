db80481a4cc9cee061a12ffb7a5ff30dd7e88a06
Juan Miguel de Haro <Juan Miguel de Haro>
2016-05-30 12:49:30 +0200

    asdasd

--- src/presentation/SettingsPresenter.java
+++ src/presentation/SettingsPresenter.java
@@ -18,6 +18,7 @@ public class SettingsPresenter extends BasePresenter  {
         SettingsView view = (SettingsView)actualView;
         try {
             UserController.changePassword(view.getCurrentPassword(), view.getNewPassword(), view.getConfirmNewPassword());
+            view.showError("Password changed successfully");
         } catch (IncorrectPassword incorrectPassword) {
             view.showError("The password you entered is not your current password");
         } catch (PasswordMissMatch passwordMissMatch) {
